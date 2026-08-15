485aab0b273879230b66d3f44107ff6e6db31642
aminulidev <aminulds61@gmail.com>
2026-08-15 21:09:00 +0600

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "CollabCanvas" }],
   icons: {
-    icon: "https://z-cdn.chatglm.cn/z-ai/static/logo.svg",
+    icon: "/logo.svg",
   },
   openGraph: {
     title: "CollabCanvas",
